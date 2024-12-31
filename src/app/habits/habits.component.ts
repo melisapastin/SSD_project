00f4e7270fe528272ed647bfe7db60b51daeda4b
melisa <melisa.pastin27@gmail.com>
2024-12-31 13:14:00 +0200

    dashboard frontend

---
+++ src/app/habits/habits.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habits',
+  imports: [],
+  templateUrl: './habits.component.html',
+  styleUrl: './habits.component.css'
+})
+export class HabitsComponent {
+
+}
